8b07e8374f18e618f7031c91cce7c0095bc5981e
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-04-07 23:25:44 +0300

    Initial commit

---
+++ src/main/java/pro/sky/gots/employees_register/EmployeesRegisterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.gots.employees_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesRegisterApplication.class, args);
+    }
+
+}

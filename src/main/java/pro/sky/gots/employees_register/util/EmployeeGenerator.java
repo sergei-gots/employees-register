af0aaefe1734bd7c0c17a7d75af1f52eebac8948
Sergei Gots <74458698+Sergei-Gots@users.noreply.github.com>
2023-06-15 02:34:28 +0300

    Update EmployeeGenerator.java

--- src/main/java/pro/sky/gots/employees_register/util/EmployeeGenerator.java
+++ src/main/java/pro/sky/gots/employees_register/util/EmployeeGenerator.java
@@ -3,13 +3,20 @@ package pro.sky.gots.employees_register.util;
 import pro.sky.gots.employees_register.model.Employee;
 
 import java.util.Random;
-
+/** Static method for genearating Employee instance 
+ *  with randomly choosen first and last names.
+ *
+ **/
 public final class EmployeeGenerator {
-
+    
     public static final Random random = new Random();
-    public static final String[] firstNames = {"Ivan", "Petr", "Pavel"};
-    public static final String[] lastNames = {"Ivanov", "Petrov", "Sidorov"};
-
+    /** Array, first names set **/
+    private static final String[] firstNames = {"Ivan", "Petr", "Pavel"};
+    /** Array, last names set **/
+    private static final String[] lastNames = {"Ivanov", "Petrov", "Sidorov"};
+    /** @return an Employee instance initiated with randomly choosen first and last names .
+     *
+     **/
     public static Employee generateEmployee() {
         return  new Employee(firstNames[random.nextInt(firstNames.length)],
                 lastNames[random.nextInt(lastNames.length)]);
